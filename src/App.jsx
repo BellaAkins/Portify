df597d29c4e5e31e150dcd532f1e9abae0ed171e
BellaAkins <sarabellaakins@gmail.com>
2025-12-09 14:37:46 +0100

    add tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+<h1 className='text-blue-900  text-5xl'>Hellooooooooo Babyyyyyyyy</h1>
+    </>
+  )
+}
+
+export default App
