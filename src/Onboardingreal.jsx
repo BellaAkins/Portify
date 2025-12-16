dccc54c367df8010aa710b7cf99aa7ae69a4cc3f
BellaAkins <sarabellaakins@gmail.com>
2025-12-16 15:02:58 +0100

    increase font sizes

--- src/Onboardingreal.jsx
+++ src/Onboardingreal.jsx
@@ -276,7 +276,7 @@ function FinalScreen() {
       <div className="flex justify-center mt-6">
         <button
           onClick={handleStart}
-          className="px-10 py-2 border border-[#1ABCFE]  text-[#1ABCFE] text-[1.5rem] rounded-xl font-semibold 
+          className="px-10 py-2 border border-[#1ABCFE]  text-[#1ABCFE] text-[1rem] rounded-xl font-semibold 
                       hover:bg-white transition"
         >
           Start Building
