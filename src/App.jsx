8cec0ae133d28515a9841383f0a31a67a6cb016b
BellaAkins <sarabellaakins@gmail.com>
2025-12-22 20:50:50 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Page from "./components/Page.jsx";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Page />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
