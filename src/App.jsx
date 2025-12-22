167da20f78886dc73c24535dbc446b036845b5a1
BellaAkins <sarabellaakins@gmail.com>
2025-12-22 19:28:57 +0100

    link login and signup to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Landingpage from "./components/Landingpage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landingpage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
